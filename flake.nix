d9b0b62495b09d12818d75d654eabbeebe7fb96f
petertjmills <ptjm8422@gmail.com>
2024-07-01 19:27:31 +0100

    feat: Add disko to nixpkgs inputs and configure nimbus system
    The code changes in `flake.nix` add `disko` to the list of nixpkgs inputs and configure the `nimbus` system in the `outputs` section. This change enables the usage of disko in the project and sets up the nimbus system with the appropriate modules and configuration.
    
    Based on the recent user commits and repository commits, it seems that the developer is actively working on improving the project's configuration and documentation.

--- flake.nix
+++ flake.nix
@@ -2,21 +2,30 @@
   description = "My nix cloud";
 
   inputs = {
-    # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
     vscode-server.url = "github:nix-community/nixos-vscode-server";
+
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, vscode-server, ... }@inputs: {
-    # Please replace my-nixos with your hostname
+  outputs = { self, nixpkgs, vscode-server, disko, ... }@inputs: {
     nixosConfigurations.cumulus = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
         vscode-server.nixosModules.default
         ./cumulus/configuration.nix
       ];
     };
+
+    nixosConfigurations.nimbus = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        disko.nixosModules.disko
+        ./nimbus/configuration.nix
+      ];
+    };
+
   };
 }
