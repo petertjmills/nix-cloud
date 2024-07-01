5eb378a0eed6a178ddd3180287451176c6687bae
petertjmills <ptjm8422@gmail.com>
2024-07-01 18:34:54 +0100

    feat: Add gnumake to package dependencies
    The code changes in `cumulus/configuration.nix` add `gnumake` to the list of package dependencies. This change is necessary to ensure that the `gnumake` command is available for building and running the project.
    
    Based on the recent user commits and repository commits, it seems that the developer is actively working on improving the project's configuration and documentation.

--- cumulus/configuration.nix
+++ cumulus/configuration.nix
@@ -20,6 +20,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    gnumake
   ];
 
   # Enable the OpenSSH daemon.
