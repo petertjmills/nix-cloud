4a6896cc9fad20eba68b2bc2b29a0d87cce9548c
petertjmills <ptjm8422@gmail.com>
2024-07-07 04:45:55 +0100

    feat: Update cumulus configuration to use "just" instead of "gnumake"
    The code changes in `cumulus/configuration.nix` update the list of installed packages to replace `gnumake` with `just`. This update enhances the cumulus configuration by using a more modern and efficient build tool.

--- cumulus/configuration.nix
+++ cumulus/configuration.nix
@@ -20,7 +20,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    gnumake
+    just
     nixpkgs-fmt
   ];
 
