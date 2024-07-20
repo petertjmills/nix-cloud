a550e00a992a2d8d3b6ab66a81230647584c07fa
petertjmills <ptjm8422@gmail.com>
2024-07-20 23:05:56 +0100

    Add cumulus ssh key

--- altostratus/configuration.nix
+++ altostratus/configuration.nix
@@ -29,6 +29,7 @@
   users.users.root.openssh.authorizedKeys.keys=[
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO8tQOhDkrQO4q3W7JdernvtL1v+aiNsjozN41qrfs2n Silversurfer"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHyxwQIShLIk/qHVnEkRWC+7/V82brDH3s0tBwpnttVi macmini"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPEhVfbVbix9lPz1+hQAeo7qRtQwIs6+ev22HLa4IiI+ root@cumulus"
   ];
 
   # Copy the NixOS configuration file and link it from the resulting system
