a575691f2b93da5e3bf106507379bc55e6f54f45
petertjmills <ptjm8422@gmail.com>
2024-07-20 23:23:24 +0100

    Make testService into a proper enablable service

--- services/test.nix
+++ services/test.nix
@@ -1,14 +1,40 @@
-{ pkgs, ... }:
-{
-  systemd.services.testService = {
-    enable = true;
-    description = "Simple Test Service";
+{ pkgs, config, ... }:
 
-    serviceConfig = {
-      ExecStart = "${pkgs.bash}/bin/bash -c 'echo \"Test service is running\" >> /var/log/test_service.log'";
-      Restart = "on-failure";
+with lib;
+
+let
+  testcfg = config.services.testService;
+in {
+  options = {
+    services.testService = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Whether to enable foo.
+        '';
+      };
+
+      logText = mkOption {
+        type = types.str;
+        default = "Test service is running";
+        description = ''
+          Text to log.
+        '';
+      };
     };
+  };
 
-    wantedBy = [ "multi-user.target" ];
+  config = mkIf testcfg.enable {
+    systemd.services.testService = {
+      description = "Simple Test Service";
+
+      serviceConfig = {
+        ExecStart = "${pkgs.bash}/bin/bash -c 'echo \"${testcfg.logText}\" >> /var/log/test_service.log'";
+        Restart = "on-failure";
+      };
+
+      wantedBy = [ "multi-user.target" ];
+    };
   };
 }
