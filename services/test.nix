0bed3273db0a230732b22694547911fe649845ce
petertjmills <ptjm8422@gmail.com>
2024-07-20 23:13:07 +0100

    Add test service to altostratus configuration
    This commit adds the test service to the altostratus configuration in the `services/test.nix` file. The test service is enabled and configured to run a bash command and restart on failure. It is also set to be wanted by the `multi-user.target`.

---
+++ services/test.nix
@@ -0,0 +1,13 @@
+{
+  services.testService = {
+    enable = true;
+    description = "Simple Test Service";
+
+    serviceConfig = {
+      ExecStart = "${pkgs.bash}/bin/bash -c 'echo \"Test service is running\" >> /var/log/test_service.log'";
+      Restart = "on-failure";
+    };
+
+    wantedBy = [ "multi-user.target" ];
+  };
+}
