a1a9f31d69334a08464362ee48150552b2b7665c
petertjmills <ptjm8422@gmail.com>
2024-09-07 12:10:05 +0100

    Cirrus init

---
+++ hosts/cirrus/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc3"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = "https://proxmox.server:8006/api2/json"
+}
+
+resource "proxmox_vm_qemu" "cirrus" {
+  name        = "cirrus"
+  target_node = "yellowsubmarine"
+  memory      = "2048"
+  cores       = "2"
+  scsihw      = "virtio-scsi-single"
+  
+  network{
+    bridge = "vmbr0"
+    link_down = false
+    firewall = true
+    model = "virtio"
+  }
+
+  disks {
+    ide {
+      ide0 {
+        cdrom {
+          iso = "local:iso/nixos-24.05.20240630.7dca152-x86_64-linux.iso"
+        }
+      }
+    }
+
+    scsi {
+      scsi0 {
+        disk {
+          size = "10G"
+          storage = "local-lvm"
+          iothread = true
+        }
+      }
+    }
+  }
+}
