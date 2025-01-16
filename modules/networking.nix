7fddd5e976bb1c9ea2b689ae26eb04c3d35c90d6
Peter Mills <56031935+petertjmills@users.noreply.github.com>
2025-01-16 08:16:18 +0000

    Hostname is set by input

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,6 +1,7 @@
-{ ip, defaultGateway, ... }:
+{ ip, defaultGateway, hostname, ... }:
 {
   networking.useDHCP = false;
+  networking.hostName = hostname;
   networking.defaultGateway = defaultGateway;
   networking.interfaces.enp1s0.ipv4.addresses = [
     {
