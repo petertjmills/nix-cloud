ba78467ffcd986061c5ba4640632f8b250516035
petertjmills <ptjm8422@gmail.com>
2025-02-13 18:56:37 +0000

    cloud-init not working

--- images/incus-lxc-base.nix
+++ images/incus-lxc-base.nix
@@ -14,6 +14,7 @@
   services.cloud-init = {
     enable = true;
     network.enable = true;
+    ext4.enable = true;
   };
   users.users.root.openssh.authorizedKeys.keyFiles = [
     ../secrets/public-keys/master_id_ed25519.pub
