b18ace9ba316b3efea513daf3944c8c5699a0b2d
petertjmills <56031935+petertjmills@users.noreply.github.com>
2025-03-15 16:44:34 +0000

    not sure

--- images/incus-vm-base.nix
+++ images/incus-vm-base.nix
@@ -18,9 +18,5 @@
   system.stateVersion = "24.11";
 
   environment.systemPackages = [
-    # pkgs.netplan # cloud-init network config does not support `on-link` routes, but will use netplan if it is available
-    # However it still doesn't work :( Should find a solution in the future. Cloud init can't find libnetplan?
-    # journalctl | grep netplan
-    # Feb 24 20:03:54 nimbostratus cloud-init[692]: 2025-02-24 20:03:54,684 - schema.py[DEBUG]: Skipping netplan schema validation. No netplan API available
   ];
 }
