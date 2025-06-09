fb9f31da5dde05d19c51e28d8cce2f5f1784a02e
petertjmills <ptjm8422@gmail.com>
2025-06-09 15:39:38 +0000

    bug: Please update nix-darwin to enable system-wide etc

--- darwin-hosts/mac-mini-m4.nix
+++ darwin-hosts/mac-mini-m4.nix
@@ -18,23 +18,23 @@ in
     mutableTaps = false;
   };
 
-  nix = {
-    package = pkgs.nix;
-    settings = {
-      trusted-users = [ "@admin" "${user}" ];
-      substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
-      trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
-    };
-    gc = {
-      automatic = true;
-      interval = { Weekday = 0; Hour = 2; Minute = 0; };
-      options = "--delete-older-than 30d";
-    };
-    # Turn this on to make command line easier
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
+  # nix = {
+  #   package = pkgs.nix;
+  #   settings = {
+  #     trusted-users = [ "@admin" "${user}" ];
+  #     substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
+  #     trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+  #   };
+  #   gc = {
+  #     automatic = true;
+  #     interval = { Weekday = 0; Hour = 2; Minute = 0; };
+  #     options = "--delete-older-than 30d";
+  #   };
+  #   # Turn this on to make command line easier
+  #   extraOptions = ''
+  #     experimental-features = nix-command flakes
+  #   '';
+  # };
 
   system = {
     # Turn off NIX_PATH warnings now that we're using flakes
